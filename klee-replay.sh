9c54161aaef39cdf5f28a18f9c317373e53d0259
suzkit <suzkit+github@gmail.com>
2019-11-27 16:33:57 +0900

    refactoring Makefile and klee-replay.sh

--- klee-replay.sh
+++ klee-replay.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "usage: $0 /path/to/binary"
+    exit 1
+fi
+
 LD_LIBRARY_PATH=/home/klee/klee_build/lib/
 for i in `ls klee-last/*ktest`; do
-    KTEST_FILE=${i} ./binarysearch
+    KTEST_FILE=${i} ./${1}
 done
